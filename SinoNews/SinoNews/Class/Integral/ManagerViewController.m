74d3d113f3c91dbe4e75575a7af7a37629cdf984
iOSJacksun <1132532777@qq.com>
2018-06-04 17:52:24 +0800

    添加积分-segment点击带渐变色

--- SinoNews/SinoNews/Class/Integral/ManagerViewController.m
+++ SinoNews/SinoNews/Class/Integral/ManagerViewController.m
@@ -7,17 +7,43 @@
 //
 
 #import "ManagerViewController.h"
+#import "LXSegmentBtnView.h"
 
 @interface ManagerViewController ()
+{
+    UIImageView *userIcon;  //头像
+    UILabel     *userName;  //昵称
+    UILabel     *integer;   //积分
+}
+
+@property (nonatomic,strong) LXSegmentBtnView *segmentView;
+@property (nonatomic,strong) CAGradientLayer *gradient;
 
 @end
 
 @implementation ManagerViewController
 
+- (CAGradientLayer *)gradient
+{
+    if (!_gradient) {
+        _gradient = [CAGradientLayer layer];
+        _gradient.frame = CGRectMake(0, 0, (ScreenW - 20)/3, 45);
+        _gradient.colors = [NSArray arrayWithObjects:
+                           (id)RGBA(79, 160, 238, 1).CGColor,
+                           (id)RGBA(173, 208, 241, 1).CGColor, nil];
+        _gradient.startPoint = CGPointMake(0, 0.5);
+        _gradient.endPoint = CGPointMake(1, 0.5);
+        _gradient.locations = @[@0.0, @1];
+    }
+    return _gradient;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = @"管理";
-    self.view.backgroundColor = Arc4randomColor;
+    self.view.backgroundColor = WhiteColor;
+    
+    [self setupTopViews];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,9 +51,78 @@
     
 }
 
+//上部分
+-(void)setupTopViews
+{
+    userIcon = [UIImageView new];
+    userName = [UILabel new];
+    userName.font = FontScale(15);
+    integer = [UILabel new];
+    integer.font = FontScale(15);
+    UIView *line = [UIView new];
+    
+    [self.view sd_addSubviews:@[
+                                      userIcon,
+                                      userName,
+                                      integer,
+                                      line,
+                                      ]];
+    userIcon.sd_layout
+    .leftSpaceToView(self.view, 10)
+    .topSpaceToView(self.view, 10)
+    .widthIs(44)
+    .heightEqualToWidth()
+    ;
+    [userIcon setSd_cornerRadius:@22];
+    userIcon.image = UIImageNamed(@"userIcon");
+    
+    userName.sd_layout
+    .leftSpaceToView(userIcon, 7)
+    .centerYEqualToView(userIcon)
+    .heightIs(kScaelW(15))
+    ;
+    [userName setSingleLineAutoResizeWithMaxWidth:150];
+    userName.text = @"春风十里不如你";
+    
+    integer.sd_layout
+    .rightSpaceToView(self.view, 10)
+    .centerYEqualToView(userIcon)
+    .heightIs(kScaelW(14))
+    ;
+    [integer setSingleLineAutoResizeWithMaxWidth:150];
+    integer.text = @"1000000积分";
+    
+    _segmentView = [LXSegmentBtnView new];
+    [self.view addSubview:_segmentView];
+    
+    _segmentView.sd_layout
+    .topSpaceToView(userIcon, 17)
+    .leftSpaceToView(self.view, 10)
+    .rightSpaceToView(self.view, 10)
+    .heightIs(45)
+    ;
+    
+    [_segmentView updateLayout];
+    _segmentView.btnTitleSelectColor = RGBA(18, 130, 238, 1);
+    _segmentView.btnTitleNormalColor = RGBA(136, 136, 136, 1);
+    WeakSelf
+    _segmentView.lxSegmentBtnSelectIndexBlock = ^(NSInteger index, UIButton *btn) {
+        [weakSelf setColorWithBtn:btn];
+    };
+    
+    _segmentView.btnTitleArray = @[
+                                   @"积分记录",
+                                   @"游戏记录",
+                                   @"兑换记录",
+                                   ];
+    
+}
 
-
-
+-(void)setColorWithBtn:(UIButton *)btn
+{
+    self.gradient.frame = CGRectMake(0, 0, (ScreenW - 20)/3, 45);
+    [btn.layer insertSublayer:self.gradient below:btn.titleLabel.layer];
+}
 
 
 
---
+++ SinoNews/SinoNews/ThirdFramwork/otherframework/LXSegmentBtn/LXSegmentBtnView.h
@@ -0,0 +1,43 @@
+//
+//  LXSegmentBtnView.h
+//  LXSegmentBtnDemo
+//
+//  Created by liuxin on 2017/12/1.
+//  Copyright © 2017年 liuxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class LXSegmentBtnView;
+@protocol LXSegmentBtnViewDelegate <NSObject>
+
+-(void)lxSegmentView:(LXSegmentBtnView *)segmentView
+         selectIndex:(NSInteger)selectIndex;
+
+@end
+
+
+typedef void(^lxSegmentBtnSelectIndexBlock)(NSInteger index,UIButton *btn);
+
+@interface LXSegmentBtnView : UIView
+
+@property (nonatomic , strong) NSArray *btnTitleArray;
+
+@property (nonatomic , strong) UIColor *btnTitleNormalColor;
+
+@property (nonatomic , strong) UIColor *btnTitleSelectColor;
+
+@property (nonatomic , strong) UIColor *btnBackgroundNormalColor;
+
+@property (nonatomic , strong) UIColor *btnBackgroundSelectColor;
+
+@property (nonatomic , strong) UIFont *titleFont;
+
+@property (nonatomic , assign) NSInteger selectedIndex;
+
+#pragma mark --block,delegate--
+@property (nonatomic , copy) lxSegmentBtnSelectIndexBlock lxSegmentBtnSelectIndexBlock;
+
+@property (nonatomic , weak) id<LXSegmentBtnViewDelegate> delegate;
+
+@end
